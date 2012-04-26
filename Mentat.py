b55180f5addfbebb3b1dfa55c5a282c1442ea032
Irving Ruan <irvingruan@gmail.com>
2012-04-25 19:26:56 -0700

    Initial commit

---
+++ Mentat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+class Mentat(object):
+    
+    def __init__(self, server, port, username, password):
+        
+        return self
+        
+
+def main():
+    print "Hello world!"
+    
+if __name__ == "__main__":
+    main()
